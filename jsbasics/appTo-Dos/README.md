878486a45ae4c409c57f9df52f5d8f976e3f7124
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2020-12-01 19:10:08 -0300

    Insert To-Dos exercice

---
+++ jsbasics/appTo-Dos/README.md
@@ -0,0 +1,3 @@
+## To-Do list created by RocketSeat website.
+
+> *dentro deste projeto, foram feitas alterações com relação aos videos fornecidos pela Rocketseat, dentro da parte de JS, como a troca dos comandos "var" por "const" por exemplo.*
---
+++ jsbasics/appTo-Dos/todos.js
@@ -0,0 +1,44 @@
+const listElement = document.querySelector('#app ul');
+const inputElement = document.querySelector('#app input');
+const buttonElement = document.querySelector('#app button');
+const todos = JSON.parse(localStorage.getItem('list_todos')) || [];
+
+function renderTodos(){
+listElement.innerHTML = '';
+  for (todo of todos) {
+    const todoElement = document.createElement('li');
+    const todoText = document.createTextNode(todo);
+    const linkElement = document.createElement('a');
+    const linkText = document.createTextNode(' Excluir');
+    const pos = todos.indexOf(todo);
+    
+    linkElement.setAttribute('href', '#');
+    linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
+    linkElement.appendChild(linkText);
+    todoElement.appendChild(todoText);
+    todoElement.appendChild(linkElement);
+    listElement.appendChild(todoElement);
+  }
+}
+
+renderTodos();
+
+function addTodo(){
+  const todoText = inputElement.value;
+  todos.push(todoText);
+  inputElement.value = '';
+  renderTodos();
+  saveToStorage();
+}
+
+buttonElement.onclick = addTodo;
+
+function deleteTodo(pos) {
+  todos.splice(pos, 1);
+  renderTodos();
+  saveToStorage();
+}
+
+function saveToStorage(){
+  localStorage.setItem('list_todos', JSON.stringify(todos));
+}
